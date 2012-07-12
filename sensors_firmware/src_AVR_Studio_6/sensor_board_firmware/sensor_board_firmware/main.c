e1e6459002b0aaca29b8f88bc5bc801cef3945da
Dmitry Votintsev <physs@ya.ru>
2012-07-12 02:10:41 -0400

    Firmware Template

---
+++ sensors_firmware/src_AVR_Studio_6/sensor_board_firmware/sensor_board_firmware/main.c
@@ -0,0 +1,17 @@
+/*
+ * sensor_board_firmware.c
+ *
+ * Created: 7/12/2012 2:07:20 AM
+ *  Author: votintdn
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
